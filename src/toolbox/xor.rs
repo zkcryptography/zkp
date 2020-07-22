9d2f407ef7c8d39e24e0a29d6dde96a3507391c8
Stephan Kemper <skemper@jhu.edu>
2020-07-22 15:59:55 -0700

    Condense internal XOR calls with Vec<&Scalar>

--- src/toolbox/xor.rs
+++ src/toolbox/xor.rs
@@ -38,10 +38,8 @@ impl<R> secrets::SecretSharing for Xor<R> where R: RngCore + CryptoRng {
         }
 
         // calculate the final share as a function of the random shares and the secret
-        match xor_many_scalars(secret, &shares) {
-            Ok(val) => shares.push(val),
-            Err(e) => return Err(e),
-        }
+        let val = xor_many_scalars(secret, shares.iter());
+        shares.push(val);
 
         return Ok(shares);
     }
@@ -49,8 +47,8 @@ impl<R> secrets::SecretSharing for Xor<R> where R: RngCore + CryptoRng {
     /// For Xor, sparse_shares should include a None value for each share you want us to generate
     fn complete(&mut self, secret: &Scalar, sparse_shares: &Vec<Option<Scalar>>) -> Result<Vec<Scalar>, String> {
         let mut empties = 0;
-        let mut shares = Vec::new();
-        let mut new_shares = Vec::new();
+        let mut shares: Vec<Scalar> = Vec::new();
+        let mut new_shares: Vec<Scalar> = Vec::new();
         for share in sparse_shares.iter() {
             if share.is_some() {
                 shares.push(share.unwrap());
@@ -69,13 +67,11 @@ impl<R> secrets::SecretSharing for Xor<R> where R: RngCore + CryptoRng {
 
         info!("Completing {} empties with {} known and secret = {:?}", empties, shares.len(), secret);
 
-        match xor_many_more_scalars(secret, &shares, &new_shares) {
-            Ok(val) => {
-                debug!("The XOR'd share is {:?}", val);
-                new_shares.push(val);
-            },
-            Err(e) => return Err(e),
-        }
+        let intermediate = xor_many_scalars(secret, shares.iter());
+        let val = xor_many_scalars(&intermediate, new_shares.iter());
+
+        debug!("The XOR'd share is {:?}", val);
+        new_shares.push(val);
 
         let ret_shares: Vec<Scalar> = sparse_shares.iter().map(|x| {
             if x.is_some() {
@@ -94,105 +90,67 @@ impl<R> secrets::SecretSharing for Xor<R> where R: RngCore + CryptoRng {
             return Err(String::from("No shares provided, impossible to reconstruct!"));
         }
 
-        let secret = xor_many_scalars(&shares[0], &shares[1..]);
-        
-        debug!("Used {} shares to reconstruct {:?}", shares.len(), secret);
-        return secret;
-    }
-}
+        let secret = xor_many_scalars(&Scalar::zero(), shares.iter());
 
-/// Given two Scalar values, return the XOR of their constituent byte arrays
-pub fn xor_scalars(a: &Scalar, b: &Scalar) -> Scalar {
-    let mut the_bytes = [0 as u8; 32];
-    let abytes = a.to_bytes();
-    let bbytes = b.to_bytes();
-    for i in 0..the_bytes.len() {
-        the_bytes[i] = abytes[i] ^ bbytes[i];
+        if secret.is_canonical() {
+            debug!("Used {} shares to reconstruct {:?}", shares.len(), secret);
+            return Ok(secret);
+        } else {
+            debug!("Value from XOR isn't within the group! {:?}", secret);
+            return Err(String::from("The reconstructed secret is outside the group"));
+        }
+
     }
-    Scalar::from_bits(the_bytes)
 }
 
 /// Given a list of scalars, XOR them all together.  We split out the `first` separately for efficiency's sake;
 /// there are use cases where the initial value you want to XOR with isn't in your Vector, so you don't need to
 /// push it.
+///
+/// The resulting value is **not** guaranteed to be modulo group order.  If this is important for your particular use
+/// case, you'll need to do those checks after calling this function.
 /// 
-/// TODO WARNING: the result isn't guaranteed to be modulo group order.  Is that going to be a problem?
 /// TODO is there a special way we need to write this function, to achieve some sort of SIMD benefit?
-pub fn xor_many_scalars(first: &Scalar, others: &[Scalar]) -> Result<Scalar, String> {
+fn xor_many_scalars<'a, T>(first: &Scalar, others: T) -> Scalar
+where
+    T: Iterator<Item=&'a Scalar>,
+{
     let mut the_bytes = first.clone().to_bytes();
-    for scalar in others.iter() {
+    for scalar in others {
         let scal_bytes = scalar.to_bytes();
         for i in 0..the_bytes.len() {
             the_bytes[i] ^= scal_bytes[i];
         }
     }
 
-    return Ok(Scalar::from_bits(the_bytes));
+    Scalar::from_bits(the_bytes)
 }
 
-/// See xor_many_scalars()
-pub fn xor_many_more_scalars(first: &Scalar, others: &Vec<Scalar>, still_others: &Vec<Scalar>) -> Result<Scalar, String> {
-    let mut the_bytes = first.clone().to_bytes();
-    for scalar in others.iter() {
-        let scal_bytes = scalar.to_bytes();
-        for i in 0..the_bytes.len() {
-            the_bytes[i] ^= scal_bytes[i];
-        }
-    }
-    for scalar in still_others.iter() {
-        let scal_bytes = scalar.to_bytes();
-        for i in 0..the_bytes.len() {
-            the_bytes[i] ^= scal_bytes[i];
-        }
-    }
-
-    return Ok(Scalar::from_bits(the_bytes));
-}
 
 #[allow(unused_imports)]
 mod tests {
     use super::*;
     use crate::toolbox::secrets::SecretSharing;
-
-    #[test]
-    fn xor_easy() {
-        let res1 = xor_scalars(&Scalar::zero(), &Scalar::zero());
-        assert_eq!(Scalar::zero(), res1);
-
-        let res2 = xor_scalars(&Scalar::zero(), &Scalar::one());
-        assert_eq!(Scalar::one(), res2);
-    }
-
-    #[test]
-    fn xor_medium() {
-        let a = Scalar::from(8675309u32);
-        let b = Scalar::from(5551212u32);
-        let c = Scalar::from(13691777u32);
-        assert_eq!(c, xor_scalars(&a, &b));
-    }
+    use curve25519_dalek::constants::BASEPOINT_ORDER;
 
     #[test]
     fn xor_many_easy() {
         let zeroes = vec![Scalar::zero(); 5];
-        let res1 = xor_many_scalars(&Scalar::zero(), &zeroes);
-        assert!(res1.is_ok());
-        assert_eq!(Scalar::zero(), res1.unwrap());
+        let res1 = xor_many_scalars(&Scalar::zero(), zeroes.iter());
+        assert_eq!(Scalar::zero(), res1);
 
-        let res2 = xor_many_scalars(&Scalar::one(), &zeroes);
-        assert!(res2.is_ok());
-        assert_eq!(Scalar::one(), res2.unwrap());
+        let res2 = xor_many_scalars(&Scalar::one(), zeroes.iter());
+        assert_eq!(Scalar::one(), res2);
     }
 
     #[test]
     fn xor_many_medium() {
         let scalars = vec![Scalar::from(8675309u32), Scalar::from(5551212u32), Scalar::from(4561414u32)];
-        let res1 = xor_many_scalars(&Scalar::zero(), &scalars);
-        assert!(res1.is_ok());
-        assert_eq!(Scalar::from(9793927u32), res1.unwrap());
+        let res1 = xor_many_scalars(&Scalar::zero(), scalars.iter());
+        assert_eq!(Scalar::from(9793927u32), res1);
 
-        let res2 = xor_many_scalars(&Scalar::from(9743901u32), &scalars);
-        assert!(res2.is_ok());
-        assert_eq!(Scalar::from(122778u32), res2.unwrap());
+        let res2 = xor_many_scalars(&Scalar::from(9743901u32), scalars.iter());
+        assert_eq!(Scalar::from(122778u32), res2);
     }
 
     #[test]
@@ -267,5 +225,20 @@ mod tests {
             },
             Err(e) => assert!(false, format!("Unable to complete: {}", e)),
         }
+    }
+
+    #[test]
+    fn xor_reconstruct_out_of_group() {
+        // 0x10 = 0000 1010             largest byte of \ell
+        // 0x5  = 0000 0101
+        //  XOR = 0000 1111
+
+        let share1 = Scalar::from_canonical_bytes([12, 227, 105, 171, 173, 162, 96, 141, 241, 244, 32, 246, 255, 9, 210, 32, 110, 245, 179, 133, 8, 34, 83, 32, 220, 162, 102, 9, 189, 38, 231, 5]).unwrap();
+        let share2 = BASEPOINT_ORDER;
+        let share3 = Scalar::from(74u8);
+
+        let mut xor = Xor::new_without_rng();
+        let shares = vec![share1, share2, share3];
+        assert!(xor.reconstruct(&shares).is_err(), "Shouldn't have been able to reconstruct!");
     }
 }
